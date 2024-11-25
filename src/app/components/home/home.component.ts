b59a47c54ea95499699aea33e50b2acb2296ac70
IvelinS <ivelinshopov@gmail.com>
2024-11-25 14:47:11 +0200

    Initial commit

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [RouterModule, CommonModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+// TODO: connect to an auth service later
+isAuthenticated = false;
+}
